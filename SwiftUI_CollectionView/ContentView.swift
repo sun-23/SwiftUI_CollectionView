2f9b03f461dd16a5b79c5542c22bccaabf4f9397
sun-23 <twinsamson@gmail.com>
2019-07-03 19:09:34 +0700

    Initial Commit

---
+++ SwiftUI_CollectionView/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_CollectionView
+//
+//  Created by sun on 3/7/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

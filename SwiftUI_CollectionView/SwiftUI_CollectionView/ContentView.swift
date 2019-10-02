edf5557d7d644b111a93c59b4484b7312eb96c35
Chaichana <49665324+sun-23@users.noreply.github.com>
2019-10-02 20:51:19 +0700

    ok

---
+++ SwiftUI_CollectionView/SwiftUI_CollectionView/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  SwiftUI_CollectionView
+//
+//  Created by sun on 3/7/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct Box {
+    
+    var id : Int
+    let title, imageUrl : String
+    
+    
+}
+
+struct ContentView : View {
+    
+    var body: some View {
+        NavigationView() {
+            RowView().navigationBarTitle("Landmark")
+        }
+    }
+}
+
+struct RowView: View {
+    
+    let boxs:[Box] = [
+        Box(id: 0, title: "stmarylake", imageUrl: "stmarylake"),
+        Box(id: 1, title: "lakemcdonald", imageUrl: "lakemcdonald"),
+        Box(id: 2, title: "turtlerock", imageUrl: "turtlerock"),
+        Box(id: 3, title: "chilkoottrail", imageUrl: "chilkoottrail"),
+        Box(id: 4, title: "yukon_charleyrivers", imageUrl: "yukon_charleyrivers")
+    ]
+    
+    var body: some View {
+        
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack(alignment: .top) {
+        
+          ForEach(boxs, id: \.id) { box in
+            
+              BoxView(Box: box)
+            
+          }
+        
+      }.padding()
+    }
+}
+}
+
+struct BoxView: View {
+    
+    let Box:Box
+    
+    var body: some View {
+        
+        ZStack {
+            Image(Box.imageUrl)
+                .resizable()
+                .frame(width:200 , height:200 )
+                .cornerRadius(12)
+            Text(Box.title)
+                .baselineOffset(-80)
+                .foregroundColor(Color.white)
+                .font(.headline)
+                .fontWeight(.bold)
+        }
+        
+    }
+    
+}
+
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
